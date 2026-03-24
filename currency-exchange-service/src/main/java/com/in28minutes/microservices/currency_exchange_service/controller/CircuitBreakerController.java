bd2bb39d5ffd4b95f359848c5c54c4c1bcec503a
vinit8879 <vinitsalvi22@gmail.com>
2026-03-24 15:29:54 +0530

    Vinit commit

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -14,12 +17,23 @@ public class CircuitBreakerController {
 
 	private Logger logger= LoggerFactory.getLogger(CircuitBreakerController.class);
 	
+//	@GetMapping("/sample-api")
+//	@Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
+//	public String sampleAPI() {
+//		logger.info("Sample api call reecieved");
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8055/demo", String.class);
+//		return forEntity.getBody();
+//	}
+	
 	@GetMapping("/sample-api")
-	@Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
+//	@CircuitBreaker(name="sample-api", fallbackMethod = "hardcodedResponse")
+//	@RateLimiter(name="sample-api")
+	@Bulkhead(name="sample-api")
 	public String sampleAPI() {
 		logger.info("Sample api call reecieved");
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8055/demo", String.class);
-		return forEntity.getBody();
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8055/demo", String.class);
+//		return forEntity.getBody();
+		return "sample-api";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
